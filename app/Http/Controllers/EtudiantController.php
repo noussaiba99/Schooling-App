cf4d07364e15f5acb6c0b9c6a8b3079e94fa5ec5
noussaiba99 <44345356+noussaiba99@users.noreply.github.com>
2019-12-31 10:23:32 +0100

    2 FORMS

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -3,51 +3,93 @@
 namespace tp_Igl\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+
+use tp_Igl\Demande;
 use tp_Igl\Etudiant;
+use tp_Igl\User;
+
 
 class EtudiantController extends Controller
 {
-    public function viewEtudiant()
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
     {
-        $etudiant=Etudiant::all();
-        $arr=Array('etudiant'=>$etudiant);
-        return view('etudiant.view',$arr);
+        $this->middleware('auth');
     }
-    public function addEtudiant(Request $request)
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
     {
-        //only excute when post
-        if ($request->isMethod('post'))
-        {  // echo $request;
-           $newetudiant=new Etudiant();
-            $newetudiant->nom=$request->input('nom');
-            $newetudiant->prenom=$request->input('prenom');
-            $newetudiant->email=$request->input('email');
-
-            $newetudiant->save();
-
-       
-        }
         return view('etudiant.add');
-
     }
-    public function modifierEtudiant(Request $request,$id)
+    public function demande(Request $request)
     {
-        //only excute when post
-        if ($request->isMethod('post'))
-        {  // echo $request;
-           $newetudiant=new Etudiant();
-            $newetudiant->nom=$request->input('nom');
-            $newetudiant->prenom=$request->input('prenom');
-            $newetudiant->email=$request->input('email');
+         //only excute when post
+         if ($request->isMethod('post'))
+         {  // echo $request;
+            if ($request->has('form1')) 
+            {
 
-            $newetudiant->save();
+             $newdemande=new Demande();
+             $emaill= auth()->user()->email;
+             $nomm=auth()->user()->nom;
+             $prenomm=auth()->user()->prenom;
+             $groupe=auth()->user()->groupe;
 
-       
-        }
-        $etudiant=Etudiant::find($id);
-        $arr=Array('etudiant'=>$etudiant);
-            return view('etudiant.modifier',$arr);
+             $newdemande->email=$emaill;
+             $newdemande->nom=$nomm;
+             $newdemande->prenom=$prenomm;
+             $newdemande->grpActuel=$groupe;
+             $newdemande->grpDest=$request->input('grpDest');
+             $newdemande->save();
+
+            
+             $user_id=User::select('id')->where('email',$emaill)->first();
+             $etudiant=User::find($user_id)->first();
+             $etudiant->permute=1;
+             $etudiant->save(); 
+
+             }
+             if ($request->has('form2')) 
+             {
+                       if (!(Hash::check($request->get('current-password'), auth()->user()->password))) {
+                    // The passwords matches
+                    return redirect()->back()->with("error","les 2 mots de passe sont pas compatibles.");
+                }
+
+                if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
+                    //Current password and new password are same
+                    return redirect()->back()->with("error","le nouveau mot de passe est exactement comme l'ancien. SVP choisissez un nouveau.");
+                }
+
+                $validatedData = $request->validate([
+                    'current-password' => 'required',
+                    'new-password' => 'required|string|min:6|confirmed',
+                ]);
+
+                //Change Password
+                $user = auth()->user();
+                $user->password = bcrypt($request->get('new-password'));
+                $user->save();
+
+                return redirect()->back()->with("success","votre mot de passe a été changé avec succés");
+             }  
+            
+         }
+        return view('etudiant.add');
     }
-    
+     
 
 }
+
