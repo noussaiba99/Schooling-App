f5e276db0270cc36d52b1b3d8843255463c7ef01
noussaiba99 <44345356+noussaiba99@users.noreply.github.com>
2019-12-31 10:40:58 +0100

    Add files via upload

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,6 +36,20 @@ class Handler extends ExceptionHandler
     {
         parent::report($exception);
     }
+    public function render($request, Exception $exception)
+{
+    // This will replace our 404 response with
+    // a JSON response.
+    if ($exception instanceof ModelNotFoundException &&
+        $request->wantsJson())
+    {
+        return response()->json([
+            'data' => 'Resource not found'
+        ], 404);
+    }
+
+    return parent::render($request, $exception);
+}
 
     /**
      * Render an exception into an HTTP response.
@@ -44,8 +58,8 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+   /* public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
-    }
+    }*/
 }
